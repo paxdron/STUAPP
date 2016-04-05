5ab6207db2fff67a56961ddd840afe7c0870c2c9
antonio <paxdron@gmail.com>
2016-04-05 02:36:50 -0500

    Con Login

---
+++ app/src/main/java/com/padron/stuapp/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.padron.stuapp;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import shem.com.materiallogin.MaterialLoginView;
+import shem.com.materiallogin.MaterialLoginViewListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+     MaterialLoginView login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        login = (MaterialLoginView) findViewById(R.id.login);
+        login.setListener(new MaterialLoginViewListener() {
+            @Override
+            public void onRegister(TextInputLayout registerUser, TextInputLayout registerPass, TextInputLayout registerPassRep) {
+                //Handle register
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+
+            @Override
+            public void onLogin(TextInputLayout loginUser, TextInputLayout loginPass) {
+                //Handle login
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/padron/stuapp/MapsActivity.java
+++ app/src/main/java/com/padron/stuapp/MapsActivity.java
@@ -1,19 +1,45 @@
 package com.padron.stuapp;
 
+import android.content.Context;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
+import com.google.maps.android.kml.KmlLayer;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.util.Arrays;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
+    private Spinner selectRecorrido;
 
+    private Gson gson = new Gson();
+    private unidadEnServicio[] current;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +48,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        cargarAdaptador();
+        selectRecorrido= (Spinner)findViewById(R.id.spSelectRuta);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.rutas,R.layout.support_simple_spinner_dropdown_item);
+        selectRecorrido.setAdapter(adapter);
+        selectRecorrido.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                showToast(Integer.toString((current != null) ? current.length : 0), getApplicationContext());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
@@ -39,8 +81,99 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng Puebla = new LatLng(19.041428, -98.206300);
+        mMap.addMarker(new MarkerOptions().position(Puebla).title("Puebla"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Puebla, 12));
+        try {
+            mMap.clear();
+            KmlLayer layer = new KmlLayer(mMap, R.raw.amalucancu, getApplicationContext());
+            layer.addLayerToMap();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
+
+    public void showToast(String message, Context context){
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+    /**
+     * Carga el adaptador con las metas obtenidas
+     * en la respuesta
+     */
+    public void cargarAdaptador() {
+        // Petición GET
+        String requestBody=null;
+        VolleySingleton.
+                getInstance(getApplicationContext()).
+                addToRequestQueue(
+                        new JsonObjectRequest(
+                                Request.Method.GET,
+                                getString(R.string.getCurrentVehicles),
+                                requestBody,
+                                new Response.Listener<JSONObject>() {
+
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        // Procesar la respuesta Json
+                                        procesarRespuesta(response);
+                                    }
+                                },
+                                new Response.ErrorListener() {
+                                    @Override
+                                    public void onErrorResponse(VolleyError error) {
+                                        Log.d(TAG, "Error Volley: " + error.getMessage());
+                                    }
+                                }
+
+                        )
+                );
+    }
+
+    /**
+     * Interpreta los resultados de la respuesta y así
+     * realizar las operaciones correspondientes
+     *
+     * @param response Objeto Json con la respuesta
+     */
+    private void procesarRespuesta(JSONObject response) {
+        try {
+            // Obtener atributo "estado"
+            String estado = response.getString("estado");
+
+            switch (estado) {
+                case "1": // EXITO
+                    // Obtener array "metas" Json
+                    JSONArray mensaje = response.getJSONArray("Current");
+                    // Parsear con Gson
+                     current = gson.fromJson(mensaje.toString(), unidadEnServicio[].class);
+                    addMarkers();
+                    break;
+                case "2": // FALLIDO
+                    String mensaje2 = response.getString("mensaje");
+                    break;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public void addMarkers(){
+
+        for (int i=0;i<current.length;i++) {
+            LatLng newPoint = new LatLng(current[i].latitud, current[i].longitud);
+
+            mMap.addMarker(new MarkerOptions().position(newPoint).title(Integer.toString(i)));
+        }
+
+        //mMap.
+    }
+
 }
